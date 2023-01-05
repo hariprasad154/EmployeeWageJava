a02e4ff1ff22f9a93c6d313194e6e19cf58d5757
Hari Prasad G <hariprasas154@gmail.com>
2023-01-05 12:42:59 +0530

    added  case and remaining files

--- EmployeWage.java
+++ EmployeWage.java
@@ -5,16 +5,16 @@ public class EmployeWage {
     static final int FULL_DAY_HOUR = 8;
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage");
-        int employeeCheck = (int) (Math.random() * 10) % 10;
+        int employeeCheck = (int) (Math.random() * 10) % 3;
         System.out.println(employeeCheck);
-//        int dailyWage = 0;
-//        if (employeeCheck == 1) {
-//            System.out.println("Employee is Present");
-//            dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-//        } else {
-//            System.out.println("Employee is Absent");
-//        }
-//        System.out.println("Daily wage => " + dailyWage);
+        int dailyWage = 0;
+        if (employeeCheck == 1) {
+            System.out.println("Employee is Present");
+            dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+        } else {
+            System.out.println("Employee is Absent");
+        }
+        System.out.println("Daily wage => " + dailyWage);
 
 	}
 
